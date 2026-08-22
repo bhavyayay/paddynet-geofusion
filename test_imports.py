9ec5c25c19c6cde2c76c98ff94e6ae2feb704f4a
Bhavya <bhavya.utbv@gmail.com>
2026-08-22 23:45:25 +0530

    Initial project structure for PaddyNet-GeoFusion Developer 1 module

---
+++ test_imports.py
@@ -0,0 +1,24 @@
+import numpy
+import pandas
+import pyarrow
+import rasterio
+import rioxarray
+import xarray
+import geopandas
+import shapely
+import sentinelhub
+import torch
+import torchvision
+import segmentation_models_pytorch
+import skimage
+import matplotlib
+from tqdm import tqdm
+
+print("numpy version:", numpy.__version__)
+print("pandas version:", pandas.__version__)
+print("rasterio version:", rasterio.__version__)
+print("geopandas version:", geopandas.__version__)
+print("torch version:", torch.__version__)
+print("segmentation_models_pytorch version:", segmentation_models_pytorch.__version__)
+print("")
+print("ALL LIBRARIES IMPORTED SUCCESSFULLY")
